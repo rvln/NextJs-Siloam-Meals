ac0d3eb20e19b2f937798aed618389b900752ec2
Frico <fravelrama88@gmail.com>
2025-09-27 03:52:35 +0800

    adjustmend dependencies and config

--- app/nurse/page.tsx
+++ app/nurse/page.tsx
@@ -1,10 +1,9 @@
-import ManagePatient from "./components/Patient"
+import ManagePatient from "./components/patient";
 
 export default function Home() {
-    return (
-        <main className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
-            <ManagePatient />
-        </main>
-    )
+  return (
+    <main className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 text-white">
+      <ManagePatient />
+    </main>
+  );
 }
- 
--- next.config.ts
+++ next.config.ts
@@ -5,14 +5,13 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8080', // PASTIKAN PORT INI SESUAI DENGAN PORT BACKEND NESTJS ANDA
-        pathname: '/public/**', // Opsional: Hanya izinkan gambar dari path /image/
+        protocol: "http",
+        hostname: "localhost",
+        port: "8080", // PASTIKAN PORT INI SESUAI DENGAN PORT BACKEND NESTJS ANDA
+        pathname: "/public/**", // Opsional: Hanya izinkan gambar dari path /image/
       },
     ],
   },
-
 };
 
 export default nextConfig;
