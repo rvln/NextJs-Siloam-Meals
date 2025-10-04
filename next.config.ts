a23910aef750f391b9617e6e5dd12e854558f7df
Frico Rama <fravelrama88@gmail.com>
2025-10-05 03:39:10 +0800

    fix hostname

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "localhost",
+        hostname: "localhost", // GANTI DENGAN IP BACKEND NESTJS ATAU LOCALHOST ANDA
         port: "8080", // PASTIKAN PORT INI SESUAI DENGAN PORT BACKEND NESTJS ANDA
         pathname: "/public/**", // Opsional: Hanya izinkan gambar dari path /image/
       },
